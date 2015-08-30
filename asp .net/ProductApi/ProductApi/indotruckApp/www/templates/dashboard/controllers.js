eded894a2ac5bb0aaf945d4edf9a5b37d5048ebb
novelaspalo <novela.spalo@gmail.com>
2015-08-30 16:53:26 +0700

    add boox

---
+++ asp .net/ProductApi/ProductApi/indotruckApp/www/templates/dashboard/controllers.js
@@ -0,0 +1,11 @@
+﻿angular.module('dashboard.controllers', [])
+
+.controller('DashboardController', function ($scope, $http, AUTH_EVENTS) {
+    $http.get("http://localhost:62827/api/product")
+        .success(function (data, status, header, config) {
+            console.log(AUTH_EVENTS.notAuthenticated);
+        })
+        .error(function (data, status, header, config) {
+            alert("error");
+        })
+});
